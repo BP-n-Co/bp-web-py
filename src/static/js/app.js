11d50eb13d1154bfc8620d0ab1d396de77acdfc1
AlixPa <81264144+AlixPa@users.noreply.github.com>
2025-05-24 17:39:02 +0900

    [BP-108] cumulative sum (#7)
    * cumulative sum
    
    * remove non usefull sub div

--- src/static/js/app.js
+++ src/static/js/app.js
@@ -1,8 +1,7 @@
 import { filterByField } from './globals/filter.js';
 import { highlightSubSequence } from './globals/subSequence.js';
-import { extractDateLabels, extractCumulAdditionsPerDay, extractModificationsPerDay, } from './globals/commits.js';
+import { extractDateLabels, extractModificationsPerDay, } from './globals/commits.js';
 window.filterByField = filterByField;
 window.highlightSubSequence = highlightSubSequence;
 window.extractDateLabels = extractDateLabels;
 window.extractModificationsPerDay = extractModificationsPerDay;
-window.extractCumulAdditionsPerDay = extractCumulAdditionsPerDay;
